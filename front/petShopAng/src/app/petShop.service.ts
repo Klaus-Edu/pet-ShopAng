7b3f9e1c1728611f7085dcf780737f11d2b5251c
Klaus Petrikovski <padilhaklaus@gmail.com>
2023-02-27 14:16:32 -0300

    tratamento de erro

--- front/petShopAng/src/app/petShop.service.ts
+++ front/petShopAng/src/app/petShop.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Pet } from './petShop';
 import { environment } from 'src/environments/environment';
-import { tap } from 'rxjs/operators'
+import { catchError, tap } from 'rxjs/operators'
 
 @Injectable({providedIn: 'root'})
 export class PetShopService {
@@ -12,19 +12,43 @@ export class PetShopService {
   constructor(private http: HttpClient){}
 
   public getOrders() : Observable<any>{
-    return this.http.get<Pet[]>(`${this.apiServerUrl}/orders/get/all`);
+    return this.http.get<Pet[]>(`${this.apiServerUrl}/orders/get/all`)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   public addOrder(pet: Pet): Observable<Pet> {
-    return this.http.post<Pet>(`${this.apiServerUrl}/orders/add`, pet);
+    return this.http.post<Pet>(`${this.apiServerUrl}/orders/add`, pet)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   public updateOrder(pet: Pet, petId: number): Observable<Pet> {
-    return this.http.put<Pet>(`${this.apiServerUrl}/orders/edit/${petId}`, pet);
+    return this.http.put<Pet>(`${this.apiServerUrl}/orders/edit/${petId}`, pet)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   public deleteOrder(petId: number | undefined): Observable<void> {
-    return this.http.delete<void>(`${this.apiServerUrl}/orders/delete/${petId}`);
+    return this.http.delete<void>(`${this.apiServerUrl}/orders/delete/${petId}`)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
+
+  handleError(error: HttpErrorResponse){
+    if(error.error instanceof ErrorEvent){//erro clientSide ou internet
+      console.error('An error ocurred: ' + error.error.message);
+    } else{
+      console.error(`Backend returned error code: ${error.status}` +
+      `body: ${error.error}`
+      )
+    }
+
+    return throwError(()=> new Error('There is an error ocurring'))
+  }
 }
